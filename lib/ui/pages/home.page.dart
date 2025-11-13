63f9d1a5d06fb47b97ac9000be401870c1c6c5e0
NicolasChusseau <nicolas.chusseau@etu.univ-nantes.fr>
2025-11-13 20:03:54 +0100

    vue des favoris

--- lib/ui/pages/home.page.dart
+++ lib/ui/pages/home.page.dart
@@ -8,6 +8,8 @@ import 'package:spacex_app/ui/cubit/display.cubit.dart';
 import 'package:spacex_app/ui/cubit/display.state.dart';
 import 'package:spacex_app/ui/widget/grid.widget.dart';
 import 'package:spacex_app/ui/widget/list.widget.dart';
+import 'package:spacex_app/ui/cubit/favorites.cubit.dart';
+import 'package:spacex_app/ui/cubit/favorites.state.dart';
 
 import '../../data/models/launch.model.dart';
 
@@ -27,72 +29,101 @@ class _HomePageState extends State<HomePage> {
   final LaunchesCubit launchCubit = LaunchesCubit();
   final DisplayCubit displayCubit = DisplayCubit();
   final DisplayFavoritesCubit displayFavoritesCubit = DisplayFavoritesCubit();
+  final FavoritesCubit favoritesCubit = FavoritesCubit();
 
   @override
   void initState() {
     WidgetsBinding.instance.addPostFrameCallback((_) => _loadData());
+    favoritesCubit.loadFavorites();
     super.initState();
   }
 
   void _loadData() => launchCubit.getData();
   void _toggleDisplay() => displayCubit.toggleDisplay();
 
-  void _toggleDisplayFavorites() => displayFavoritesCubit.toggleDisplayFavorites();
+  Future<void> _toggleDisplayFavorites() async {
+    await favoritesCubit.loadFavorites();
+    displayFavoritesCubit.toggleDisplayFavorites();
+  }
+
+  @override
+  void dispose() {
+    launchCubit.close();
+    displayCubit.close();
+    displayFavoritesCubit.close();
+    favoritesCubit.close();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<DisplayCubit, DisplayCubitState>(
       bloc: displayCubit,
-      builder: (BuildContext context, DisplayCubitState displayCubit) {
+      builder: (BuildContext context, DisplayCubitState displayState) {
         return BlocBuilder<DisplayFavoritesCubit, DisplayFavoritesCubitState>(
           bloc: displayFavoritesCubit,
-          builder: (BuildContext context, DisplayFavoritesCubitState displayFavoritesCubit) {
-            return Scaffold(
-                appBar: AppBar(
-                  title: const Text('SpaceX Launches'),
-                  actions: [
-                    IconButton(
-                        onPressed: _toggleDisplayFavorites,
-                        icon: Icon(
-                            displayFavoritesCubit.showFavoritesOnly
-                                ? Icons.star
-                                : Icons.star_border
-                        )
+          builder: (BuildContext context, DisplayFavoritesCubitState favoritesDisplayState) {
+            return BlocBuilder<FavoritesCubit, FavoritesState>(
+              bloc: favoritesCubit,
+              builder: (BuildContext context, FavoritesState favoritesState) {
+                return Scaffold(
+                    appBar: AppBar(
+                      title: const Text('SpaceX Launches'),
+                      actions: [
+                        IconButton(
+                            onPressed: _toggleDisplayFavorites,
+                            icon: Icon(
+                                favoritesDisplayState is DisplayFavoritesState
+                                    ? Icons.star
+                                    : Icons.star_border
+                            )
+                        ),
+                        IconButton(
+                          onPressed: _toggleDisplay,
+                          tooltip: 'Changer la disposition',
+                          icon: Icon(
+                              displayState is GridState ? Icons.view_list : Icons.grid_view),
+                        ),
+                      ],
                     ),
-                    IconButton(
-                      onPressed: _toggleDisplay,
-                      tooltip: 'Changer la disposition',
-                      icon: Icon(
-                          displayCubit.isGridView ? Icons.view_list : Icons
-                              .grid_view),
-                    ),
-                  ],
-                ),
-                body: BlocBuilder<LaunchesCubit, CubitState<List<Launch>>>(
-                    bloc: launchCubit,
-                    builder: (BuildContext context,
-                        CubitState<List<Launch>> launchListCubit) {
-                      if (launchListCubit is FailureState) {
-                        return Center(
-                          child: Text(
-                              'Error : ${(launchListCubit as FailureState)
-                                  .message}'),
-                        );
-                      } else
-                      if (launchListCubit is SuccessState<List<Launch>>) {
-                        List<Launch> launches = launchListCubit.data;
-                        if (displayCubit is ListState) {
-                          return ListWidget(launches: launches);
-                        } else {
-                          return GridWidget(launches: launches);
-                        }
-                      }
-                      else {
-                        return const Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      }
-                    })
+                    body: BlocBuilder<LaunchesCubit, CubitState<List<Launch>>>(
+                        bloc: launchCubit,
+                        builder: (BuildContext context,
+                            CubitState<List<Launch>> launchListCubit) {
+                          if (launchListCubit is FailureState) {
+                            return Center(
+                              child: Text(
+                                  'Error : ${(launchListCubit as FailureState).message}'),
+                            );
+                          } else if (launchListCubit is SuccessState<List<Launch>>) {
+                            List<Launch> launches = launchListCubit.data;
+
+                            List<Launch> displayedLaunches = launches;
+                            if (favoritesDisplayState is DisplayFavoritesState) {
+                              displayedLaunches = launches
+                                  .where((l) => favoritesCubit.isFavorite(l.id))
+                                  .toList();
+                            }
+
+                            if (displayState is ListState) {
+                              return BlocProvider.value(
+                                value: favoritesCubit,
+                                child: ListWidget(launches: displayedLaunches),
+                              );
+                            } else {
+                              return BlocProvider.value(
+                                value: favoritesCubit,
+                                child: GridWidget(launches: displayedLaunches),
+                              );
+                            }
+                          } else {
+                            return const Center(
+                              child: CircularProgressIndicator(),
+                            );
+                          }
+                        })
+                );
+              },
             );
           }
         );
